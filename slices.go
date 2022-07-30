e9039348b20a73fbd4f8924d8d928359ed97b508
Bowen Zhu <promacanthus@gmail.com>
2022-07-30 16:15:06 +0800

    Add annotation

--- slices.go
+++ slices.go
@@ -1,5 +1,6 @@
 package generics
 
+// Filter returns any list of elements according to the boolean value of the function.
 func Filter[T any](s []T, f func(T) bool) []T {
 	var res []T
 	if len(s) == 0 {
